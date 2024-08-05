c945ba3cde606d2f658aeaefd61f8d8793a6d427
Wang Ruochen <wrc@ruo-chen.wang>
2024-08-05 14:14:52 -0400

    clean comments

--- yabai-zsh-completions.rb
+++ yabai-zsh-completions.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class YabaiZshCompletions < Formula
   desc "Zsh completion for yabai"
   homepage "https://github.com/Amar1729/yabai-zsh-completions"
